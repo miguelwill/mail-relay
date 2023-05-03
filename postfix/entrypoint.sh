14eccb46e4c56a1fdf7fdded333d16b6e134186d
HomeServerHQ <drdoug@cloudcitylab.com>
2023-05-03 14:15:49 -0500

    Updated everything

---
+++ postfix/entrypoint.sh
@@ -0,0 +1,312 @@
+#!/bin/bash
+
+# Only on container creation
+INITIALIZED="/.initialized"
+if [ ! -f "$INITIALIZED" ]; then
+
+  cat <<EOFHC >  /container/scripts/docker-healthcheck.sh
+#!/bin/bash
+
+# Check for changes to certificates, reload postfix if different.
+diff /etc/postfix/tls /tmp/tls
+if [ \$? -ne 0 ]; then
+  postfix reload
+  rm -rf /tmp/tls 2> /dev/null
+  cp -a /etc/postfix/tls /tmp/tls
+  echo "SSL certificates updated, postfix reloaded..."
+fi
+
+[[ $(ps aux | grep '[r]unsvdir\|[r]syslogd\|[s]bin/master' | wc -l) -ge '3' ]]
+exit \$?
+EOFHC
+  chmod 755 /container/scripts/docker-healthcheck.sh
+
+  SSL_CERT_FILENAME=cert.pem
+  if [ ! -z ${LETS_ENCRYPT_CERT_FILENAME+x} ]; then
+    SSL_CERT_FILENAME=$LETS_ENCRYPT_CERT_FILENAME
+  fi
+  SSL_KEY_FILENAME=key.pem
+  if [ ! -z ${LETS_ENCRYPT_KEY_FILENAME+x} ]; then
+    SSL_KEY_FILENAME=$LETS_ENCRYPT_KEY_FILENAME
+  fi
+  if [ -z ${MAIL_FQDN+x} ] || [ -z ${POSTMASTER_ADDRESS+x} ]; then
+    echo "Missing required environment variables or certificates, exiting..."
+    exit 1
+  fi
+  if [ ! -f /etc/postfix/tls/$SSL_KEY_FILENAME ]; then
+    openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout /etc/postfix/tls/$SSL_KEY_FILENAME -out /etc/postfix/tls/$SSL_CERT_FILENAME -subj "/C=US/ST=MO/L=STL/O=HomeServerHQ/OU=SampleCert/CN=$MAIL_FQDN"
+  fi
+
+  rm -rf /tmp/tls 2> /dev/null
+  cp -a /etc/postfix/tls /tmp/tls
+
+  MAIL_FQDN=$(echo "$MAIL_FQDN" | sed 's/[^.0-9a-z\-]//g')
+  MAIL_NAME=$(echo "$MAIL_FQDN" | cut -d'.' -f1)
+  MAILDOMAIN=$(echo "$MAIL_FQDN" | cut -d'.' -f2-)
+
+  echo "Setting mail host to: $MAIL_FQDN"
+  echo "$MAIL_FQDN" > /etc/mailname
+  echo "$MAIL_NAME" > /etc/hostname
+
+  QUEUE_LIFETIME_BOUNCE=7d
+  QUEUE_LIFETIME_MAX=7d
+
+  if [ ! -z ${POSTFIX_QUEUE_LIFETIME_BOUNCE+x} ]; then
+    echo "POSTFIX set bounce_queue_lifetime = $POSTFIX_QUEUE_LIFETIME_BOUNCE"
+    QUEUE_LIFETIME_BOUNCE=$POSTFIX_QUEUE_LIFETIME_BOUNCE
+  fi
+
+  if [ ! -z ${POSTFIX_QUEUE_LIFETIME_MAX+x} ]; then
+    echo "POSTFIX set maximal_queue_lifetime = $POSTFIX_QUEUE_LIFETIME_MAX"
+    QUEUE_LIFETIME_MAX=$POSTFIX_QUEUE_LIFETIME_MAX
+  fi
+
+  if [ ! -f /etc/postfix/config/transport ]; then
+    echo "Transport map is empty, no emails will be relayed. Creating empty file..."
+    touch /etc/postfix/config/transport
+  fi
+  postmap /etc/postfix/config/transport
+
+  if [ ! -f /etc/postfix/config/relay ]; then
+    echo "No relay domains are specified, no emails will be relayed. Creating empty file..."
+    touch /etc/postfix/config/relay
+  fi
+  postmap /etc/postfix/config/relay
+
+  if [ -z ${ABUSE_ADDRESS+x} ]; then
+    ABUSE_ADDRESS=$POSTMASTER_ADDRESS
+  fi
+
+  # Must have postmaster and abuse accounts enabled to be RFC compliant
+  cat <<EOF >  /etc/postfix/virtual
+postmaster    $POSTMASTER_ADDRESS
+abuse    $ABUSE_ADDRESS
+EOF
+  postmap /etc/postfix/virtual
+
+  if [ ! -f /etc/postfix/config/header_checks ]; then
+    echo "No header checks file. Creating empty file..."
+    touch /etc/postfix/config/header_checks
+  fi
+
+  if [ ! -f /etc/postfix/config/sasl_passwd ]; then
+    echo "No forward relay hosts credentials file. Creating empty file..."
+    touch /etc/postfix/config/sasl_passwd
+  fi
+  postmap /etc/postfix/config/sasl_passwd
+
+  dh1024_file=/etc/postfix/dh1024.pem
+  dh512_file=/etc/postfix/dh512.pem
+
+  if [ -f /etc/postfix/tls/dh1024.pem ]; then
+    dh1024_file=/etc/postfix/tls/dh1024.pem
+  fi
+
+  if [ -f /etc/postfix/tls/dh512.pem ]; then
+    dh512_file=/etc/postfix/tls/dh512.pem
+  fi
+
+  if [ ! -f /etc/postfix/sasl/smtpd.conf ]; then
+    cat <<EOF > /etc/postfix/sasl/smtpd.conf
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+mech_list: PLAIN LOGIN
+sasldb_path: /etc/postfix/sasl/sasldb2
+EOF
+  fi
+
+  if [ ! -f /etc/postfix/sasl/sasldb2 ]; then
+    tmppw=$(openssl rand -hex 32)
+    echo $tmppw | saslpasswd2 -c -p -f /etc/postfix/sasl/sasldb2 tempuser
+    saslpasswd2 -d -f /etc/postfix/sasl/sasldb2 tempuser
+    chown postfix:sasl /etc/postfix/sasl/sasldb2
+  fi
+
+  cat <<EOF > /etc/postfix/main-new.cf
+###### Host Settings ######
+
+smtpd_banner = $MAIL_FQDN ESMTP
+myhostname = $MAIL_FQDN
+
+###### General Settings ######
+
+biff = no
+append_dot_mydomain = no
+readme_directory = no
+compatibility_level = 2
+mailbox_size_limit = 0
+recipient_delimiter = +
+inet_interfaces = all
+inet_protocols = all
+mydestination = 
+relayhost = 
+mynetworks = 
+alias_maps = hash:/etc/aliases
+alias_database = hash:/etc/aliases
+local_recipient_maps = 
+local_transport = error:local mail delivery is disabled
+transport_maps = hash:/etc/postfix/config/transport
+relay_domains = hash:/etc/postfix/config/relay
+virtual_alias_maps = hash:/etc/postfix/virtual
+smtpd_helo_required = yes
+bounce_queue_lifetime = $QUEUE_LIFETIME_BOUNCE
+maximal_queue_lifetime = $QUEUE_LIFETIME_MAX
+milter_protocol = 6
+milter_default_action = accept
+smtpd_milters = inet:postfix-gateway-rspamd:11332
+non_smtpd_milters = $smtpd_milters
+milter_mail_macros =  i {mail_addr} {client_addr} {client_name} {auth_authen}
+
+###### Restrictions ######
+
+smtpd_helo_restrictions = 
+    reject_invalid_helo_hostname,
+    reject_non_fqdn_helo_hostname,
+    reject_unknown_helo_hostname
+
+smtpd_sender_restrictions = 
+    reject_non_fqdn_sender,
+    reject_unlisted_sender,
+    reject_unauth_destination,
+    reject_unknown_sender_domain,
+    reject_unauth_pipelining
+
+smtpd_recipient_restrictions = 
+    reject_unauth_destination,
+    reject_unknown_sender_domain,
+    reject_unauth_pipelining
+
+smtpd_relay_restrictions = 
+    reject_unauth_destination
+
+##### Outgoing Relay Settings #####
+
+smtp_sasl_auth_enable = yes
+smtp_sasl_security_options = noanonymous
+smtp_sasl_password_maps = hash:/etc/postfix/config/sasl_passwd
+
+##### TLS Settings ######
+
+# Outgoing Connections #
+
+smtp_tls_security_level = may
+smtp_tls_cert_file = /etc/postfix/tls/$SSL_CERT_FILENAME
+smtp_tls_key_file = /etc/postfix/tls/$SSL_KEY_FILENAME
+smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt
+smtp_tls_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtp_tls_mandatory_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtp_tls_mandatory_ciphers = high
+smtp_tls_protocols = TLSv1.3, TLSv1.2, !TLSv1.1, !TLSv1, !SSLv2, !SSLv3
+smtp_tls_mandatory_protocols = TLSv1.3, TLSv1.2, !TLSv1.1, !TLSv1, !SSLv2, !SSLv3
+smtp_tls_fingerprint_digest = sha256
+smtp_tls_session_cache_database = btree:\${data_directory}/smtp_scache
+smtp_tls_loglevel = 1
+
+# Incoming Connections #
+
+smtpd_tls_security_level=may
+smtpd_tls_cert_file = /etc/postfix/tls/$SSL_CERT_FILENAME
+smtpd_tls_key_file = /etc/postfix/tls/$SSL_KEY_FILENAME
+smtpd_tls_CAfile = /etc/ssl/certs/ca-certificates.crt
+smtpd_tls_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtpd_tls_mandatory_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtpd_tls_mandatory_ciphers = high
+smtpd_tls_protocols = TLSv1.3, TLSv1.2, !TLSv1.1, !TLSv1, !SSLv2, !SSLv3
+smtpd_tls_mandatory_protocols = TLSv1.3, TLSv1.2, !TLSv1.1, !TLSv1, !SSLv2, !SSLv3
+smtpd_tls_fingerprint_digest = sha256
+smtpd_tls_session_cache_database = btree:\${data_directory}/smtpd_scache
+smtpd_tls_loglevel = 1
+smtpd_tls_dh1024_param_file = $dh1024_file
+smtpd_tls_dh512_param_file = $dh512_file
+
+EOF
+
+  cp /etc/postfix/master.cf /etc/postfix/master-new.cf
+  cat <<EOF >> /etc/postfix/master-new.cf
+
+submission inet n - n - - smtpd
+ -o syslog_name=postfix/submission
+ -o smtpd_tls_security_level=encrypt
+ -o smtpd_tls_auth_only=yes
+ -o smtpd_tls_req_ccert=yes
+ -o smtpd_reject_unlisted_recipient=yes
+ -o smtpd_client_restrictions=
+ -o smtpd_helo_restrictions=
+ -o smtpd_sender_restrictions=reject_sender_login_mismatch
+ -o smtpd_recipient_restrictions=permit_sasl_authenticated,reject
+ -o smtpd_relay_restrictions=permit_sasl_authenticated,reject
+ -o milter_macro_daemon_name=ORIGINATING
+ -o smtpd_milters=
+ -o cleanup_service_name=submissioncleanup
+ -o smtpd_tls_CAfile=/etc/postfix/tls/$INTERNAL_CA_CERT_FILENAME
+ -o smtpd_sasl_auth_enable=yes
+ -o smtpd_sasl_security_options=noanonymous
+ -o cyrus_sasl_config_path=/etc/postfix/sasl
+ -o smtpd_sasl_local_domain=mail-gateway
+ -o smtpd_sender_login_maps=hash:/etc/postfix/config/sasl_senders
+ -o content_filter=
+
+EOF
+
+  # Performs header checks for submission emails in separate cleanup process
+  cat <<EOF >> /etc/postfix/master-new.cf
+submissioncleanup unix n - - - 0 cleanup
+ -o header_checks=regexp:/etc/postfix/config/header_checks
+ -o mime_header_checks=regexp:/etc/postfix/config/header_checks
+EOF
+
+  if [ ! -f /etc/postfix/config/sasl_senders ]; then
+    touch /etc/postfix/config/sasl_senders
+  fi
+  postmap /etc/postfix/config/sasl_senders
+
+  # POSTFIX RAW Config ENVs
+  if env | grep '^POSTFIX_RAW_CONFIG_'
+  then
+    echo -e "\n## POSTFIX_RAW_CONFIG ##\n" >> /etc/postfix/main-new.cf
+    env | grep '^POSTFIX_RAW_CONFIG_' | while read I_CONF
+    do
+      CONFD_CONF_NAME=$(echo "$I_CONF" | cut -d'=' -f1 | sed 's/POSTFIX_RAW_CONFIG_//g' | tr '[:upper:]' '[:lower:]')
+      CONFD_CONF_VALUE=$(echo "$I_CONF" | sed 's/^[^=]*=//g')
+      echo "$CONFD_CONF_NAME""=""$CONFD_CONF_VALUE" >> /etc/postfix/main-new.cf
+    done
+  fi
+
+  # Replace main.cf and master.cf
+  mv /etc/postfix/main-new.cf /etc/postfix/main.cf
+  mv /etc/postfix/master-new.cf /etc/postfix/master.cf
+
+  # RUNIT
+  echo "RUNIT - enable services"
+  mkdir /container/runit/postfix
+  mkdir /container/runit/rsyslog
+  cat <<EOFRU >> /container/runit/postfix/run
+#!/bin/sh
+service postfix start; sleep 5; while ps aux | grep [p]ostfix | grep [m]aster > /dev/null 2> /dev/null; do sleep 5; done
+EOFRU
+  cat <<EOFRU >> /container/runit/postfix/finish
+#!/bin/sh
+service postfix stop
+EOFRU
+  cat <<EOFRU >> /container/runit/rsyslog/run
+#!/bin/sh
+exec /usr/sbin/rsyslogd -n
+EOFRU
+  cat <<EOFRU >> /container/runit/rsyslog/finish
+#!/bin/sh
+rm /var/run/rsyslogd.pid
+EOFRU
+  chmod 755 /container/runit/postfix/run
+  chmod 755 /container/runit/postfix/finish
+  chmod 755 /container/runit/rsyslog/run
+  chmod 755 /container/runit/rsyslog/finish
+  ln -s /container/runit/postfix /etc/service/postfix
+  ln -s /container/runit/rsyslog /etc/service/rsyslog
+
+  touch "$INITIALIZED"
+fi
+
+# CMD
+echo "CMD: exec docker CMD"
+echo "$@"
+exec "$@"
---
+++ rspamd/entrypoint.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+if ! [ -f /etc/rspamd/local.d/dkim_signing.conf ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/dkim_signing.conf
+enabled = false;
+EOFDK
+fi
+if ! [ -f /etc/rspamd/local.d/logging.inc ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/logging.inc
+type = "console";
+level = "notice";
+EOFDK
+fi
+if ! [ -f /etc/rspamd/local.d/milter_headers.conf ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/milter_headers.conf
+use = ["authentication-results", "x-spam-status"];
+authenticated_headers = ["authentication-results"];
+EOFDK
+fi
+if ! [ -f /etc/rspamd/local.d/redis.conf ] && ! [ -z $REDIS_HOST ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/redis.conf
+servers = "$REDIS_HOST";
+EOFDK
+fi
+if ! [ -f /etc/rspamd/local.d/worker-normal.inc ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/worker-normal.inc
+enabled = false;
+EOFDK
+fi
+if ! [ -f /etc/rspamd/local.d/worker-controller.inc ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/worker-controller.inc
+type = "controller";
+bind_socket = "*:11334";
+EOFDK
+  if [ -z $WEB_GUI_PASSWORD_HASH ]; then
+    cat <<EOFDK >> /etc/rspamd/local.d/worker-controller.inc
+password = "$WEB_GUI_PASSWORD_HASH";
+enable_password = "$WEB_GUI_PASSWORD_HASH";
+EOFDK
+  fi
+fi
+if ! [ -f /etc/rspamd/local.d/worker-proxy.inc ]; then
+  cat <<EOFDK > /etc/rspamd/local.d/worker-proxy.inc
+milter = yes;
+bind_socket = "0.0.0.0:11332";
+timeout = 120s;
+upstream "local" {
+  default = yes;
+  self_scan = yes;
+}
+count = 4;
+max_retries = 5;
+discard_on_reject = true;
+quarantine_on_reject = false;
+spam_header = "X-Spam";
+reject_message = "Spam message rejected";
+EOFDK
+fi
+if [ -z $CLAMAV_HOST ]; then
+  rm -f /etc/rspamd/local.d/antivirus.conf
+else
+ cat <<EOFAV > /etc/rspamd/local.d/antivirus.conf
+
+clamav {
+  scan_mime_parts = true;
+  symbol = "CLAM_VIRUS";
+  type = "clamav";
+  servers = "$ANTIVIRUS_HOST:3310";
+  
+}
+
+EOFAV
+fi
+if ! [ -f /etc/rspamd/local.d/phishing.conf ] && ! [ -z $IS_ENABLE_PHISHING ]; then
+  cat <<EOFPH > /etc/rspamd/local.d/phishing.conf
+openphish_enabled = true;
+phishtank_enabled = true;
+EOFPH
+fi
+
+cat <<EOFNG > /etc/nginx/nginx.conf
+worker_processes  2;
+user nginx nginx;
+
+pid        /var/run/nginx.pid;
+
+error_log /dev/stdout info;
+
+events {
+        worker_connections 8192;
+        use epoll;
+}
+
+http {
+    include       mime.types;
+    default_type  text/plain;
+
+    sendfile  on;
+    tcp_nopush   on;
+    tcp_nodelay on;
+
+    gzip  on;
+
+    server {
+        access_log /dev/stdout;
+
+        location / {
+            alias /usr/share/rspamd/www/;
+            try_files \$uri @proxy;
+        }
+        location @proxy {
+                proxy_pass  http://127.0.0.1:11334;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_set_header Host \$http_host;
+        }
+        server_tokens off;
+    }
+}
+EOFNG
+
+cat <<EOFSV > /etc/supervisord/supervisord.conf
+[supervisord]
+nodaemon=true
+user=root
+
+[program:rspamd]
+command=/usr/sbin/rspamd -f -u rspamd -g rspamd
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+
+[program:nginx]
+command=/usr/sbin/nginx -g 'daemon off;'
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOFSV
+
+exec "$@"
